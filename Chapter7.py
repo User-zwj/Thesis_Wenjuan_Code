50a60a9276c54838bf409fbe6400502ef7145193
Wenjuan <wenjuan.zhang66@gmail.com>
2021-04-30 01:22:43 +0000

    Add import os in chap7

--- Chapter7.py
+++ Chapter7.py
@@ -2,6 +2,7 @@
 ### This file is used to generate Figures & Tables in Chapter7 ###
 ##################################################################
 
+import os
 import time
 import numpy as np
 import tensorflow as tf
